9662efaa55ce7fe80acd31b38fb4379450ef5285
Merve Arta <merve.arta@gmail.com>
2024-02-29 16:26:12 +0300

    first commit

---
+++ TabBarUygulamasi/AyarlarSayfaVC.swift
@@ -0,0 +1,19 @@
+//
+//  AyarlarSayfaVC.swift
+//  TabBarUygulamasi
+//
+//  Created by Merve Arta on 27.02.2024.
+//
+
+import UIKit
+
+class AyarlarSayfaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
